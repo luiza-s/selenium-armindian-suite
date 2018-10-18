8578886f46708a6d89ce4b4f31fcfeedeab019ba
luiza-s <luizasuryan@gmail.com>
2018-10-18 20:42:38 +0400

    my selenum 1st test project

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,28 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
 
 public class LoginTest {
 
+    private WebDriver driver;
     @Test
     public void successLogin(){
         System.setProperty("webdriver.chrome.driver", "/Users/studentb/BrowserDriver/chromedriver_win32/chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
+        driver= new ChromeDriver();
+        driver.get("https://google.com");
+
+
+        assertTrue(driver.getCurrentUrl().contains("https://www.google.com"), "incorrect page");
+        driver.quit();
     }
+    @AfterMethod
+    public void closeBrwoser(){
+        driver.quit();
+    }
+
 
 }
